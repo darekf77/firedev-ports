fbf44ff4fb32d4c8cd63ce65b676b1bf76ae496f
Dariusz Filipiak <dariusz.filipiak@igt.com>
2022-05-02 13:17:29 +0200

    update

--- projects/standalone-example/components/foo/foo.component.ts
+++ projects/standalone-example/components/foo/foo.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Morphi } from 'morphi';
 // import {  } from 'isomorphic-lib/module/asd/asdasd/bbb'
 // import {  } from 'isomorphic-lib/module/dupa/asd'
 
